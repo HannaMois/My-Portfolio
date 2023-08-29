3f4dfaee99ff314f2cded5b65186d76cd0744c5e
HannaMois <hannamois135@gmail.com>
2023-08-29 10:00:54 +0300

    Add changes

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import { projects } from 'constants/projects';
-// import { Container, WrapperBox } from './ProjectsPage.styled';
-
-// const MyProjects = () => {
-//   return (
-//     <Container>
-//       {projects.map(({ id, title, description, image, code, website }) => {
-//         return (
-//           <WrapperBox key={id}>
-//             <div>
-//               <div>
-//                 <img src={image} alt="project" width="100%" height="247px" />
-//               </div>
-//               <div>
-//                 <h3>{title}</h3>
-//               </div>
-//               <p>{description}</p>
-//             </div>
-//             <div>
-//               <a href={website} target="_blank" rel="noreferrer">
-//                 Website
-//               </a>
-//               <a href={code} target="_blank" rel="noreferrer">
-//                 Code on GitHub
-//               </a>
-//             </div>
-//           </WrapperBox>
-//         );
-//       })}
-//     </Container>
-//   );
-// };
-
-// export default MyProjects;
-
 import React from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import { projects } from 'constants/projects';
