45346ba7e5d843633c84f258714a212b39d3fd0d
HannaMois <hannamois135@gmail.com>
2023-08-29 09:35:30 +0300

    Create components/App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './Header/Header';
+// import Loader from './Loader/Loader';
+
+// const ProjectDetailsPage = lazy(() => import('pages/ProjectDetailsPage'));
+const MyProjects = lazy(() => import('pages/ProjectsPage'));
+const HomePage = lazy(() => import('pages/HomePage'));
+const LoaderPage = lazy(() => import('pages/LoaderPage'));
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {/* <Suspense fallback={<Loader />}> */}
+      <Header />
+      <Suspense>
+        {/* <Loader /> */}
+
+        <Routes>
+          <Route path="/" element={<LoaderPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/projects/:projectId" element={<MyProjects />} />
+          {/* <Route path="/details" element={<ProjectDetailsPage />} /> */}
+        </Routes>
+      </Suspense>
+    </>
   );
-};
+}
