28eba38718323fdf3132bcbef551b36505e623c5
HannaMois <hannamois135@gmail.com>
2023-09-02 11:58:24 +0300

    Add photo

---
+++ src/images/index.js
@@ -0,0 +1,51 @@
+import SlimMom from './SlimMom.jpg';
+import Wallet from './Wallet.jpg';
+import wallet1 from './wallet1.jpg';
+import wallet3 from './wallet3.jpg';
+import wallet4 from './wallet4.jpg';
+import wallet6 from './wallet5.jpg';
+import wallet7 from './wallet7.jpg';
+import slim1 from './slim1.jpg';
+import slim4 from './slim4.jpg';
+import slim5 from './slim5.jpg';
+import slim6 from './slim6.jpg';
+import slim7 from './slim7.jpg';
+import task1 from './task1.jpg';
+import task3 from './task3.jpg';
+import task4 from './task4.jpg';
+import task5 from './task5.jpg';
+import task6 from './task6.jpg';
+import task7 from './task7.jpg';
+import film1 from './film1.jpg';
+import film2 from './film2.jpg';
+import film3 from './film3.jpg';
+import film4 from './film4.jpg';
+import film5 from './film5.jpg';
+import film6 from './film6.jpg';
+
+export {
+  SlimMom,
+  Wallet,
+  wallet1,
+  wallet3,
+  wallet4,
+  wallet6,
+  wallet7,
+  film1,
+  film2,
+  film3,
+  film4,
+  film5,
+  film6,
+  task1,
+  task3,
+  task4,
+  task5,
+  task6,
+  task7,
+  slim1,
+  slim7,
+  slim6,
+  slim5,
+  slim4,
+};
